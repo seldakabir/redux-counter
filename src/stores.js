05f15e6c37dd84f223e47e346e671daef07ab523
selda kabir <seldaakabirnejat@gmail.com>
2024-09-13 15:44:16 +0300

    useselecctor

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,4 @@
-import React from "react";
 import { combineReducers, createStore } from "redux";
-import customerSlice from "./Features/customers/customerSlice";
-import accountSlice from "./Features/accounts/accountSlice";
 import accountReducer from "./Features/accounts/accountSlice";
 import customerReducer from "./Features/customers/customerSlice";
 const storeCombine = combineReducers({
@@ -11,7 +8,4 @@ const storeCombine = combineReducers({
 
 const store = createStore(storeCombine);
 store.dispatch({ type: "account/deposit", payload: 200 });
-console.log(store.getState());
-export default function Store() {
-  return <div>Store</div>;
-}
+export default store;
